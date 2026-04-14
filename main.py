15a18c302c49a053a25ee8d80d432f6873d5a1e9
Niccolo Discacciati <niccolo.discacciati@gmail.com>
2026-04-14 19:22:33 +0200

    Adding secrets

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.subheader(f"This is copied from streamlit by {st.secrets['my_username']} :sunglasses:")
+
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
