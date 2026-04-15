e713f1c32c842c8e31bbc86bd23cb1133dd65715
Niccolo Discacciati <niccolo.discacciati@gmail.com>
2026-04-15 12:21:15 +0200

    Adding GA component beautifulsoup

--- main.py
+++ main.py
@@ -7,27 +7,64 @@ st.title('Uber pickups in NYC')
 st.subheader(f"This is copied from streamlit by {st.secrets['my_username']} :sunglasses:")
 
 #Adding a streamlit component (st.components.v1 is replaced by st.iframe)
-html_code = """
-<div style="padding:20px; background-color:#f0f2f6; border-radius:10px;">
-    <h2 style="color:#4CAF50;">Hello Streamlit 👋</h2>
-    <p>This is a custom HTML component inside Streamlit.</p>
-    <button onclick="alert('Button clicked!')">Click Me</button>
-</div>
-"""
-st.iframe(html_code, height=200)
+# html_code = """
+# <div style="padding:20px; background-color:#f0f2f6; border-radius:10px;">
+#     <h2 style="color:#4CAF50;">Hello Streamlit 👋</h2>
+#     <p>This is a custom HTML component inside Streamlit.</p>
+#     <button onclick="alert('Button clicked!')">Click Me</button>
+# </div>
+# """
+# st.iframe(html_code, height=200)
 
-GA_code = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PBXS9SB8FP"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+# GA_code = """
+# <!-- Google tag (gtag.js) -->
+# <script async src="https://www.googletagmanager.com/gtag/js?id=G-PBXS9SB8FP"></script>
+# <script>
+#     window.dataLayer = window.dataLayer || [];
+#     function gtag(){dataLayer.push(arguments);}
+#     gtag('js', new Date());
 
-  gtag('config', 'G-PBXS9SB8FP');
-</script>
-"""
-st.iframe(GA_code, height=200)
+#   gtag('config', 'G-PBXS9SB8FP');
+# </script>
+# """
+# st.iframe(GA_code, height=200)
+
+# Inject GA as suggested in https://discuss.streamlit.io/t/how-to-add-google-analytics-or-js-code-in-a-streamlit-app/1610/38
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
+
+def inject_ga():
+    GA_ID = "google_analytics"
+    GA_JS = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PBXS9SB8FP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+      gtag('config', 'G-PBXS9SB8FP');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+# Call the actual function
+inject_ga()
 
 
 
