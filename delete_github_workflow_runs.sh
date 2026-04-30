b8d39f2cc14bd7a9744ae154eea36b170c3644f7
Niccolo Discacciati <niccolo.discacciati@gmail.com>
2026-04-30 17:23:38 +0200

    Sh script to delete workflow files from yml

---
+++ delete_github_workflow_runs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WORKFLOW_FILE="$1"
+echo "Working on workflow file: $WORKFLOW_FILE"
+
+# Get all run IDs
+run_ids=$(gh run list --limit 500 --workflow "$WORKFLOW_FILE" --json databaseId -q '.[].databaseId')
+
+# Exit if no runs found
+if [[ -z "$run_ids" ]]; then
+  echo "No workflow runs found."
+  exit 0
+fi
+
+# Loop through each run ID
+for id in $run_ids; do
+  echo "Processing run ID: $id"
+
+  # Delete the run
+  gh run delete "$id"
+done
